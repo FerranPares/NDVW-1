3c7b3219a3798411e64faef321fb2ab303f073ae
AlcoholNahMas <cucaverda@hotmail.com>
2015-10-19 20:20:36 +0200

    intent de fer el script de moures

---
+++ islandDemoCosa/Assets/Scripts/Gorilla.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gorilla : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ islandDemoCosa/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+    GameObject spawnpoint;
+    
+  
+
+    // Use this for initialization
+    void Start () {
+        spawnpoint=GameObject.FindGameObjectWithTag("Gorilla");
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Hello", gameObject);
+        
+        this.transform.position = spawnpoint.transform.position;
+    }
+}
