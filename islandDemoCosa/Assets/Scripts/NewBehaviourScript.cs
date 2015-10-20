feada497aaa37112f7e902e88c49668bedb0859f
Ferran <ferran.pares.pont@gmail.com>
2015-10-20 18:16:20 +0200

    movment! :D (this tim for real)

--- islandDemoCosa/Assets/Scripts/NewBehaviourScript.cs
+++ islandDemoCosa/Assets/Scripts/NewBehaviourScript.cs
@@ -1,26 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
-    private GameObject spawnpoint;
+public class NewBehaviourScript : MonoBehaviour
+{
     private GameObject gorila;
-    public int cosa = 0;
+    private Vector3 direction;
+    private float distance;
+    public float velocity = 0.01f;
 
-   
+
 
     // Use this for initialization
-    void Start () {
-        gorila=GameObject.FindGameObjectWithTag("Gorilla");
-        
-
-
+    void Start()
+    {
+        gorila = GameObject.FindGameObjectWithTag("Gorilla");
     }
-	
-	// Update is called once per frame
-	void Update () {
-        
-        Debug.Log("Hello", gameObject);
-        
-        this.transform.position = gorila.transform.position;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 diff_vector = gorila.transform.position - this.transform.position;
+        direction = Vector3.Normalize(diff_vector);
+        distance = Vector3.Magnitude(diff_vector);
+        this.transform.position = this.transform.position + direction * distance * velocity;
     }
 }
