416ab63997543ebfe39d75112714133b07531df1
Ferran <ferran.pares.pont@gmail.com>
2015-11-24 18:32:12 +0100

    basic

---
+++ BasicProject/Assets/Custom_Scripts/Bunny.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnitySteer.Behaviors;
+
+[RequireComponent(typeof(SteerForPoint))]
+public class Bunny : MonoBehaviour {
+
+	public SteerForPoint cSteering;
+	public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		cSteering = GetComponent<SteerForPoint> ();
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(cSteering.ReportedMove && !cSteering.ReportedArrival){
+			animator.SetBool ("move", true);
+		}else{
+			animator.SetBool ("move", false);
+		}
+	}
+}
