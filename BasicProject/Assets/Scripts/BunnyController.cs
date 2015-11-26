beba95dbd5792cb8a738f0f2fcd7762da03d1e90
Ferran <ferran.pares.pont@gmail.com>
2015-11-26 23:12:40 +0100

    some changes
    animations now goes pretty fine!
    camera: 2 implemented ways to control camera
    player: is playable with mouse and keyboard
    GOING for scripts and obstacles!

---
+++ BasicProject/Assets/Scripts/BunnyController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnitySteer.Behaviors;
+
+[RequireComponent(typeof(SteerForPoint))]
+public class BunnyController : MonoBehaviour {
+
+	public SteerForPoint cSteering;
+	public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		cSteering = GetComponent<SteerForPoint> ();
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(cSteering.ReportedMove && !cSteering.ReportedArrival){
+			animator.SetBool ("IsWalking", true);
+		}else{
+			animator.SetBool ("IsWalking", false);
+		}
+	}
+}
