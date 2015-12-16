e5af4fca425552ba78e39fc9c9c46ef271030cbd
Ferran <ferran.pares.pont@gmail.com>
2015-12-16 17:50:20 +0100

    blah

--- BasicProject/Assets/AI/Actions/SpawnZombunny.cs
+++ BasicProject/Assets/AI/Actions/SpawnZombunny.cs
@@ -3,17 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using RAIN.Action;
 using RAIN.Core;
+using RAIN.Representation;
 
 [RAINAction]
 public class SpawnZombunny : RAINAction
 {
+
+	public Expression position = new Expression();
+	private Vector3 _spawnPosition;
+	private GameObject _god;
+
     public override void Start(RAIN.Core.AI ai)
     {
         base.Start(ai);
+		_spawnPosition = (Vector3) position.Evaluate<Vector3> (ai.DeltaTime, ai.WorkingMemory);
+		_god = GameObject.FindGameObjectsWithTag("God");
     }
 
     public override ActionResult Execute(RAIN.Core.AI ai)
     {
+		EnemyManager enemyManager = _god.GetComponent<EnemyManager> ();
+		enemyManager.BunnySpawn (_spawnPosition);
         return ActionResult.SUCCESS;
     }
 
