a53fc5e96d03b0ccee841543438d3bc91a1523e1
Ferran <ferran.pares.pont@gmail.com>
2015-12-16 17:43:35 +0100

    spawning bunny avaliable

--- BasicProject/Assets/Scripts/Managers/EnemyManager.cs
+++ BasicProject/Assets/Scripts/Managers/EnemyManager.cs
@@ -2,21 +2,27 @@
 
 public class EnemyManager : MonoBehaviour
 {
-    public GameObject egg;
+    public GameObject _egg;
+	public GameObject _bunny;
     public float spawnTime = 3f;
 	private Vector3 EggPosition;
 
     void Start ()
     {
-        InvokeRepeating ("Spawn", spawnTime, spawnTime);
+        InvokeRepeating ("EggSpawn", spawnTime, spawnTime);
     }
 
-    void Spawn ()
+    void EggSpawn ()
     {
 		Vector3 position_offset = transform.position;
 		EggPosition.Set (Random.Range (-50, 50), 0, Random.Range (-50, 50));
 		Vector3 spawnPoint = position_offset + EggPosition;
 
-		Instantiate(egg, spawnPoint, Quaternion.identity);
+		Instantiate(_egg, spawnPoint, Quaternion.identity);
     }
+
+	public void BunnySpawn(Vector3 spawnPoint){
+		// SpawnPoint passed by Armand -> probably exact same position than Hellephant: problem?
+		Instantiate(_bunny, spawnPoint, Quaternion.identity);
+	}
 }
